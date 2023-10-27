cb7dc61f7783722a74d4edbf6619ec8191c710a1
iamshobh <iamshobhraj@gmail.com>
2023-10-27 17:39:33 +0530

    react+tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl pt-48 font-bold text-sky-600 hover:text-pink-600 transition duration-300 text-center'>REACT+TAILWIND</h1>
+    </>
+  )
+}
+
+export default App
