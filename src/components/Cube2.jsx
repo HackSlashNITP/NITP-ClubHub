8661af5a7e39d8e8f8c53a970df1a10de60fed61
ritikkr066 <ritikkrsiwan345@gmail.com>
2023-11-22 01:27:39 +0530

    height fix

--- src/components/Cube2.jsx
+++ src/components/Cube2.jsx
@@ -1,16 +1,15 @@
 
 const Cube2 = (props) => {
    
-    return (
-      <>
-      <div className="flex flex-col items-center justify-center mx-2 my-4 space-y-2 text-white">
-         <img src="/box.svg" width={50} height={50} alt="box"/>
-        <p className="text-xl font-bold">{props.name}</p>
-        <p>{props.disp}</p>
-        </div> 
-      </>
-    )
-  }
-  
-  export default Cube2
-  
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center mx-2 my-4 space-y-2 text-white">
+      <div className="my-4"> <img src="/box.svg"  width={50} height={50} alt="box"/></div>
+      <p className="text-xl font-bold">{props.name}</p>
+      <p>{props.disp}</p>
+      </div> 
+    </>
+  )
+}
+
+export default Cube2
