a17c2c310364ff2be51205cc20b0442f8bd18091
ritikkr066 <ritikkrsiwan345@gmail.com>
2023-11-22 01:12:33 +0530

    height changes

--- src/components/Cube2.jsx
+++ src/components/Cube2.jsx
@@ -4,7 +4,7 @@ const Cube2 = (props) => {
     return (
       <>
       <div className="flex flex-col items-center justify-center mx-2 my-4 space-y-2 text-white">
-         <img src="/box.svg" width={50} height={50} alt="box"/>
+        <div className="my-4"> <img src="/box.svg"  width={50} height={50} alt="box"/></div>
         <p className="text-xl font-bold">{props.name}</p>
         <p>{props.disp}</p>
         </div> 
--- src/hero-section4/hs4-event.jsx
+++ src/hero-section4/hs4-event.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Event = (props) => {
   return (
-    <main className="md:w-1/3 w-4/5 lg:px-6 md:px-4">
-      <div className="bg-slate-400 w-full h-60 md:mb-6 mb-3">IMAGE</div>
-      <div className="text-lg md:text-xl lg:text-2xl font-semibold md:mb-4 mb-2">
+    <main className="w-4/5 md:w-1/3 lg:px-6 md:px-4">
+      <div className="w-full mb-3 bg-slate-400 h-60 md:mb-6">IMAGE</div>
+      <div className="mb-2 text-lg font-semibold md:text-xl lg:text-2xl md:mb-4">
         {props.title}
       </div>
       <div className="text-neutral-400 md:text-base lg:text-lg">
         {props.info}
       </div>
-      <div className="text-neutral-500 text-sm md:pt-4 pt-2 mb-8 cursor-pointer">
+      <div className="pt-2 mb-8 text-sm cursor-pointer text-neutral-500 md:pt-4">
         Learn More
       </div>
     </main>
